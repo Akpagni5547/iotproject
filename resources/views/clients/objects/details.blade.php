936dc542cf978d7bf54d54659e5b9d0a00d2e235
Akpagni5547 <akpagniaugustin@gmail.com>
2024-04-04 03:50:45 +0000

    feat(client): add realtime

--- resources/views/clients/objects/details.blade.php
+++ resources/views/clients/objects/details.blade.php
@@ -116,6 +116,33 @@
                             </div><!-- end card -->
                         </div><!-- end col -->
                     @endif
+                        @if( $type == "Captor" || $type == "Both")
+                            <div class="col-xl-3 col-md-6">
+                                <!-- card -->
+                                <div class="card card-animate">
+                                    <div class="card-body">
+                                        <div class="d-flex align-items-center">
+                                            <div class="flex-grow-1 overflow-hidden">
+                                                <p class="text-uppercase fw-medium text-muted text-truncate mb-0">
+                                                    Donnée actuelle</p>
+                                            </div>
+                                        </div>
+                                        <div class="d-flex align-items-end justify-content-between mt-4">
+                                            <div id="realtime-object">
+                                                <div class="spinner-border" role="status">
+                                                    <span class="sr-only">Loading...</span>
+                                                </div>
+                                            </div>
+                                            <div class="avatar-sm flex-shrink-0">
+                                    <span class="avatar-title bg-soft-primary rounded fs-3">
+                                        <i class="bx bx-sun text-primary"></i>
+                                    </span>
+                                            </div>
+                                        </div>
+                                    </div><!-- end card body -->
+                                </div><!-- end card -->
+                            </div><!-- end col -->
+                        @endif
                 </div>
 
                 @if( $type == "Captor" || $type == "Both")
